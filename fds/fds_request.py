991d817cc54f564f5708bf24a2280364a4f24307
shenyuannan <shenyuannan@xiaomi.com>
2015-02-28 00:21:56 +0800

    added fds_request beloning to last commit

---
+++ fds/fds_request.py
@@ -0,0 +1,61 @@
+#wrap the requests library
+
+import requests
+
+class FDSRequest:
+  def __init__(self, timeout, max_retries):
+    from requests.sessions import HTTPAdapter
+    self._adapter = HTTPAdapter(max_retries = max_retries)
+    self._timeout = timeout
+
+  def request(self, method, url, kwargs):
+    '''
+    Constructs a specified session and sends quest.
+    Returns :class:`Response <Response>` object.
+    '''
+
+    kwargs.setdefault('timeout', self._timeout)
+    session = requests.Session()
+    session.mount("http://", self._adapter)
+    session.mount("https://", self._adapter)
+    response = session.request(method=method, url=url, **kwargs)
+    # By explicitly closing the session, we avoid leaving sockets open which
+    # can trigger a ResourceWarning in some cases, and look like a memory leak
+    # in others.
+    session.close()
+    return response
+
+
+  def get(self, url, **kwargs):
+    kwargs.setdefault('allow_redirects', True)
+    return self.request('get', url, kwargs)
+
+
+  def options(self, url, **kwargs):
+    kwargs.setdefault('allow_redirects', True)
+    return self.request('options', url, kwargs)
+
+
+  def head(self, url, **kwargs):
+    kwargs.setdefault('allow_redirects', False)
+    return self.request('head', url, kwargs)
+
+
+  def post(self, url, data=None, json=None, **kwargs):
+    kwargs['data'] = data
+    kwargs['json'] = json
+    return self.request('post', url, kwargs)
+
+
+  def put(self, url, data=None, **kwargs):
+    kwargs['data'] = data
+    return self.request('put', url,  kwargs)
+
+
+  def patch(self, url, data=None, **kwargs):
+    kwargs['data'] = data
+    return self.request('patch', url,  kwargs)
+
+
+  def delete(self, url, **kwargs):
+    return self.request('delete', url, kwargs)
