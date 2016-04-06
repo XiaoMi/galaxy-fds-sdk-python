5e21a9c168bed87d4f624dcb31a30e5673d045a9
shenjiaqi <shenjiaqi@xiaomi.com>
2016-04-06 11:27:57 +0800

    add new file

---
+++ fds/model/init_multipart_upload_result.py
@@ -0,0 +1,8 @@
+class InitMultipartUploadResult(object):
+  '''
+  The Init Multipart Upload Result class:
+  '''
+  def __init__(self, json):
+    self.bucket_name = json['bucketName']
+    self.object_name = json['objectName']
+    self.upload_id = json['uploadId']
---
+++ fds/model/upload_part_result.py
@@ -0,0 +1,32 @@
+class UploadPartResult(dict):
+  '''
+  The Upload Part Result class.
+  '''
+  def __init__(self, json):
+    self.partNumber = json['partNumber']
+    self.etag = json['etag']
+    self.partSize = json['partSize']
+
+  @property
+  def partNumber(self):
+    return self.partNumber
+
+  @partNumber.setter
+  def partNumber(self, part_number):
+    self['partNumber'] = part_number
+
+  @property
+  def etag(self):
+    return self.etag
+
+  @etag.setter
+  def etag(self, etag):
+    self['etag'] = etag
+
+  @property
+  def partSize(self):
+    return self.partSize
+
+  @partSize.setter
+  def partSize(self, part_size):
+    self['partSize'] = part_size
